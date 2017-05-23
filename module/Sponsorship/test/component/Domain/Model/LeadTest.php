c346874fd4dcbcaac606b24ba06fa9ae97200201
Carnage <t.carnage@gmail.com>
2017-05-23 23:22:26 +0100

    Cleaned up tests

--- module/Sponsorship/test/component/Domain/Model/LeadTest.php
+++ module/Sponsorship/test/component/Domain/Model/LeadTest.php
@@ -2,55 +2,29 @@
 
 namespace ConferenceTools\Sponsorship\Domain\Model;
 
-use Carnage\Cqrs\Aggregate\Identity\GeneratorInterface;
-use Carnage\Cqrs\MessageBus\MessageBusInterface;
-use Carnage\Cqrs\MessageBus\MessageInterface;
-use Carnage\Cqrs\Persistence\EventStore\InMemoryEventStore;
-use Carnage\Cqrs\Persistence\Repository\AggregateRepository;
 use ConferenceTools\Sponsorship\Domain\Command\Lead\AcquireLead;
 use ConferenceTools\Sponsorship\Domain\CommandHandler\Lead as LeadCommandHandler;
 use ConferenceTools\Sponsorship\Domain\Event\Lead\LeadAcquired;
 use ConferenceTools\Sponsorship\Domain\Model\Lead\Lead;
 use ConferenceTools\Sponsorship\Domain\ValueObject\Contact;
-use PHPUnit\Framework\TestCase;
-use Zend\Log\Logger;
-use Zend\Log\Writer\Noop;
+use ConferenceTools\Sponsorship\Testing\AbstractBusTest;
 
-class LeadTest extends TestCase
+class LeadTest extends AbstractBusTest
 {
+    protected $modelClass = Lead::class;
+
     public function testLeadAcquired()
     {
-        $idGenerator = new class implements GeneratorInterface {
-            public $lastId = 0;
-            public function generateIdentity()
-            {
-                $this->lastId++;
-                return (string) $this->lastId;
-            }
-        };
-
-        $messageBus = new class implements MessageBusInterface {
-            public $messages;
-            public function dispatch(MessageInterface $message)
-            {
-                $this->messages[] = $message;
-            }
-        };
-
-        $eventStore = new InMemoryEventStore();
-        $repository = new AggregateRepository(Lead::class, $eventStore, $messageBus);
-        $logger = (new Logger())->addWriter(new Noop());
-
-        $sut = new LeadCommandHandler($repository, $idGenerator);
-        $sut->setLogger($logger);
+        $sut = new LeadCommandHandler($this->repository, $this->idGenerator);
+        $this->setupLogger($sut);
 
         $contact = new Contact('Zoe', 'zoe@awesomesponsor.com', '01233 123456');
         $command = new AcquireLead('Awesome sponsor', $contact);
 
         $sut->handle($command);
 
-        self::assertCount(1, $messageBus->messages);
-        $domainMessage = $messageBus->messages[0]->getEvent();
+        self::assertCount(1, $this->messageBus->messages);
+        $domainMessage = $this->messageBus->messages[0]->getEvent();
 
         self::assertInstanceOf(LeadAcquired::class, $domainMessage);
         self::assertSame($contact, $domainMessage->getContact());
--- module/Sponsorship/test/component/Domain/Process/ConversationTest.php
+++ module/Sponsorship/test/component/Domain/Process/ConversationTest.php
@@ -2,12 +2,6 @@
 
 namespace ConferenceTools\Sponsorship\Domain\Process;
 
-use Carnage\Cqrs\Aggregate\Identity\GeneratorInterface;
-use Carnage\Cqrs\Event\DomainMessage;
-use Carnage\Cqrs\MessageBus\MessageBusInterface;
-use Carnage\Cqrs\MessageBus\MessageInterface;
-use Carnage\Cqrs\Persistence\EventStore\InMemoryEventStore;
-use Carnage\Cqrs\Persistence\Repository\AggregateRepository;
 use ConferenceTools\Sponsorship\Domain\Command\AlarmClock\SendAt;
 use ConferenceTools\Sponsorship\Domain\Command\Conversation\EscalateReply;
 use ConferenceTools\Sponsorship\Domain\Event\Conversation\MessageReceived;
@@ -16,20 +10,16 @@ use ConferenceTools\Sponsorship\Domain\Event\Conversation\ReplyTimeout;
 use ConferenceTools\Sponsorship\Domain\ProcessManager\Conversation as ConversationProcessManager;
 use ConferenceTools\Sponsorship\Domain\ValueObject\Contact;
 use ConferenceTools\Sponsorship\Domain\ValueObject\Message;
-use PHPUnit\Framework\TestCase;
-use Zend\Log\Logger;
-use Zend\Log\Writer\Noop;
+use ConferenceTools\Sponsorship\Testing\AbstractBusTest;
 
-class ConversationTest extends TestCase
+class ConversationTest extends AbstractBusTest
 {
+    protected $modelClass = Conversation::class;
+
     public function test_it_should_delay_a_reply_timeout_message_when_a_message_is_received()
     {
-        $idGenerator = $this->makeIdGenerator();
-        $messageBus = $this->makeMessageBus();
-        $eventStore = new InMemoryEventStore();
-
-        $repository = $this->makeRepository($messageBus, $eventStore);
-        $sut = $this->makeProcessManager($repository, $idGenerator);
+        $sut = new ConversationProcessManager($this->repository, $this->idGenerator);
+        $this->setupLogger($sut);
 
         $event = new MessageReceived(
             '1',
@@ -39,8 +29,8 @@ class ConversationTest extends TestCase
 
         $sut->handle($event);
 
-        self::assertCount(2, $messageBus->messages);
-        $domainMessage = $messageBus->messages[0]->getEvent();
+        self::assertCount(2, $this->messageBus->messages);
+        $domainMessage = $this->messageBus->messages[0]->getEvent();
 
         self::assertInstanceOf(SendAt::class, $domainMessage);
         self::assertInstanceOf(ReplyTimeout::class, $domainMessage->getMessage());
@@ -55,20 +45,17 @@ class ConversationTest extends TestCase
                 new Message('Subject', 'Body')
             ),
         ];
+        $this->given(Conversation::class, '1', $previousEvents);
 
-        $idGenerator = $this->makeIdGenerator();
-        $messageBus = $this->makeMessageBus();
-        $eventStore = $this->makeEventStore(Conversation::class, '1', $previousEvents);
-
-        $repository = $this->makeRepository($messageBus, $eventStore);
-        $sut = $this->makeProcessManager($repository, $idGenerator);
+        $sut = new ConversationProcessManager($this->repository, $this->idGenerator);
+        $this->setupLogger($sut);
 
         $event = new ReplyTimeout('1');
 
         $sut->handle($event);
 
-        self::assertCount(2, $messageBus->messages);
-        $domainMessage = $messageBus->messages[0]->getEvent();
+        self::assertCount(2, $this->messageBus->messages);
+        $domainMessage = $this->messageBus->messages[0]->getEvent();
 
         self::assertInstanceOf(EscalateReply::class, $domainMessage);
     }
@@ -84,96 +71,18 @@ class ConversationTest extends TestCase
             new MessageSent('1', new Message('Re: Subject', 'Body'))
         ];
 
-        $idGenerator = $this->makeIdGenerator();
-        $messageBus = $this->makeMessageBus();
-        $eventStore = $this->makeEventStore(Conversation::class, '1', $previousEvents);
+        $this->given(Conversation::class, '1', $previousEvents);
 
-        $repository = $this->makeRepository($messageBus, $eventStore);
-        $sut = $this->makeProcessManager($repository, $idGenerator);
+        $sut = new ConversationProcessManager($this->repository, $this->idGenerator);
+        $this->setupLogger($sut);
 
         $event = new ReplyTimeout('1');
 
         $sut->handle($event);
 
-        self::assertCount(1, $messageBus->messages);
-        $domainMessage = $messageBus->messages[0]->getEvent();
+        self::assertCount(1, $this->messageBus->messages);
+        $domainMessage = $this->messageBus->messages[0]->getEvent();
 
         self::assertInstanceOf(ReplyTimeout::class, $domainMessage);
-    }
-
-    /**
-     * @return mixed
-     */
-    private function makeIdGenerator()
-    {
-        return new class implements GeneratorInterface
-        {
-            public $lastId = 0;
-
-            public function generateIdentity()
-            {
-                $this->lastId++;
-                return (string)$this->lastId;
-            }
-        };
-    }
-
-    /**
-     * @return mixed
-     */
-    private function makeMessageBus()
-    {
-        $messageBus = new class implements MessageBusInterface
-        {
-            public $messages;
-
-            public function dispatch(MessageInterface $message)
-            {
-                $this->messages[] = $message;
-            }
-        };
-        return $messageBus;
-    }
-
-    /**
-     * @param $messageBus
-     * @return AggregateRepository
-     */
-    private function makeRepository($messageBus, $eventStore): AggregateRepository
-    {
-        $repository = new AggregateRepository(Conversation::class, $eventStore, $messageBus);
-        return $repository;
-    }
-
-    /**
-     * @param $repository
-     * @param $idGenerator
-     * @return ConversationProcessManager
-     */
-    private function makeProcessManager($repository, $idGenerator): ConversationProcessManager
-    {
-        $logger = (new Logger())->addWriter(new Noop());
-
-        $sut = new ConversationProcessManager($repository, $idGenerator);
-        $sut->setLogger($logger);
-        return $sut;
-    }
-
-    /**
-     * @return InMemoryEventStore
-     */
-    private function makeEventStore($class, $id, $events): InMemoryEventStore
-    {
-        $domainMessages = [];
-        $version = 1;
-
-        foreach ($events as $event) {
-            $domainMessages[] = DomainMessage::recordEvent($class, $id, $version, $event);
-            $version++;
-        }
-
-        $eventStore = new InMemoryEventStore();
-        $eventStore->save($class, $id, $domainMessages);
-        return $eventStore;
     }
 }
