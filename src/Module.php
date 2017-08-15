015c7f2fd6fb0623be17391d9606da3b1b556c88
Carnage <t.carnage@gmail.com>
2017-08-15 08:34:17 +0100

    Added code to change layout by module

--- src/Module.php
+++ src/Module.php
@@ -2,8 +2,22 @@
 
 namespace ConferenceTools\Sponsorship;
 
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\ModuleManager;
+use Zend\Mvc\Controller\AbstractActionController;
+
 class Module
 {
+    public function init(ModuleManager $mm)
+    {
+        $mm->getEventManager()->getSharedManager()->attach(AbstractActionController::class,
+            'dispatch', function(EventInterface $e) {
+                $controller = $e->getTarget();
+                $namespace = strtolower(explode('\\', get_class($controller))[1]);
+                $controller->layout($namespace . '/layout');
+            });
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
